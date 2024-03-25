280544779d854183215995e8683c267cc60e5b41
Ajsen Martynov <ajsmartmin@gmail.com>
2024-03-25 18:52:32 +0900

    Create translation.js

---
+++ src/locales/ru/translation.js
@@ -0,0 +1,10 @@
+export default {
+  translation: {
+    feedback: {
+      errors: {
+        invalid_url: 'Ссылка должна быть валидным URL',
+        existing_url: 'RSS уже существует',
+      },
+    },
+  },
+};
