eb2bed6372c3adcb566c1ee218e7a78f5083e3e5
Ajsen Martynov <ajsmartmin@gmail.com>
2024-04-11 11:39:03 +0900

    Update view.js

--- src/view.js
+++ src/view.js
@@ -1,33 +1,65 @@
-import { isEmpty } from 'lodash';
+// @ts-check
 
-const renderFeedback = (form, input, i18nextInstance, state, errors, previousErrors) => {
-  form.reset();
-  input.focus();
-
-  const hadError = !isEmpty(previousErrors);
-  const hasError = !isEmpty(errors);
-
-  if (!hadError && !hasError) {
-    return;
-  }
-
-  const feedback = document.querySelector('.feedback');
-
-  if (!hasError && hadError) {
-    input.classList.remove('is-invalid');
-    feedback.textContent = '';
-    return;
-  }
-
-  input.classList.add('is-invalid');
-  feedback.textContent = i18nextInstance.t(state.form.errors);
+const renderSuccessFeedback = (elements, i18nextInstance) => {
+  const { formUrlInput, feedbackParagraph } = elements;
+  formUrlInput.classList.remove('is-invalid');
+  feedbackParagraph.classList.replace('text-danger', 'text-success');
+  feedbackParagraph.textContent = i18nextInstance.t('feedback.rss_loaded');
 };
 
-export default (form, input, i18nextInstance, state) => (path, value, previousValue) => {
-  // console.log(path, value, previousValue);
+const renderErrorFeedback = (elements, i18nextInstance, error) => {
+  const { formUrlInput, feedbackParagraph } = elements;
+
+  if (!error) {
+    formUrlInput.classList.remove('is-invalid');
+    feedbackParagraph.textContent = '';
+    return;
+  }
+
+  formUrlInput.classList.add('is-invalid');
+  feedbackParagraph.classList.add('text-danger');
+  feedbackParagraph.textContent = i18nextInstance.t(error.message);
+};
+
+const handleProcessState = (elements, i18nextInstance, processState) => {
+  const { urlForm, urlFormSubmitButton, formUrlInput } = elements;
+  switch (processState) {
+    case 'loading':
+      urlFormSubmitButton.disabled = true;
+      formUrlInput.disabled = true;
+      break;
+    case 'loaded':
+      urlFormSubmitButton.disabled = false;
+      formUrlInput.disabled = false;
+      renderSuccessFeedback(elements, i18nextInstance);
+      urlForm.reset();
+      formUrlInput.focus();
+      break;
+    case 'failed':
+      urlFormSubmitButton.disabled = false;
+      formUrlInput.disabled = false;
+      formUrlInput.focus();
+      break;
+    case 'filling':
+      urlFormSubmitButton.disabled = false;
+      formUrlInput.disabled = false;
+      break;
+    default:
+      throw new Error(`Unknown 'processState': ${processState}`);
+  }
+};
+
+export default (elements, i18nextInstance, state) => (path, value) => {
+  const { urlFormSubmitButton } = elements;
   switch (path) {
-    case 'form.errors':
-      renderFeedback(form, input, i18nextInstance, state, value, previousValue);
+    case 'feedLoadingProcess.state':
+      handleProcessState(elements, i18nextInstance, value);
+      break;
+    case 'feedLoadingProcess.validationState':
+      urlFormSubmitButton.disabled = value === 'invalid';
+      break;
+    case 'feedLoadingProcess.error':
+      renderErrorFeedback(elements, i18nextInstance, value);
       break;
     default:
       break;
