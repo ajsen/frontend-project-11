d9b1aa8b5ee083d04f364be103e8fe64d2d74daf
Ajsen Martynov <ajsmartmin@gmail.com>
2024-04-11 11:35:21 +0900

    Update translation.js

--- src/locales/ru/translation.js
+++ src/locales/ru/translation.js
@@ -1,9 +1,13 @@
+// @ts-check
+
 export default {
   translation: {
     feedback: {
+      rss_loaded: 'RSS успешно загружен',
       errors: {
         invalid_url: 'Ссылка должна быть валидным URL',
-        existing_url: 'RSS уже существует',
+        existing_rss: 'RSS уже существует',
+        invalid_rss: 'Ресурс не содержит валидный RSS',
       },
     },
   },
