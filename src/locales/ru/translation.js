2d3affb9072d543912d402dc4017a24c60abf68c
Ajsen Martynov <ajsmartmin@gmail.com>
2024-04-24 21:34:46 +0900

    Add translations

--- src/locales/ru/translation.js
+++ src/locales/ru/translation.js
@@ -8,7 +8,14 @@ export default {
         invalid_url: 'Ссылка должна быть валидным URL',
         existing_rss: 'RSS уже существует',
         invalid_rss: 'Ресурс не содержит валидный RSS',
+        network_error: 'Ошибка сети',
+        required_field: 'Не должно быть пустым',
       },
+    },
+    news_feed: {
+      feeds: 'Фиды',
+      posts: 'Посты',
+      view: 'Просмотр',
     },
   },
 };
