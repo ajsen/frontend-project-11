779c141049ec0b5e22ab938124449c88d7beb7e1
Ajsen Martynov <ajsmartmin@gmail.com>
2024-04-26 00:56:12 +0900

    Refactor translation.js

--- src/locales/ru/translation.js
+++ src/locales/ru/translation.js
@@ -2,20 +2,28 @@
 
 export default {
   translation: {
-    feedback: {
-      rss_loaded: 'RSS успешно загружен',
-      errors: {
-        invalid_url: 'Ссылка должна быть валидным URL',
-        existing_rss: 'RSS уже существует',
-        invalid_rss: 'Ресурс не содержит валидный RSS',
-        network_error: 'Ошибка сети',
-        required_field: 'Не должно быть пустым',
+    rss_form: {
+      feedback: {
+        success: {
+          rss_loaded: 'RSS успешно загружен',
+        },
+        errors: {
+          invalid_url: 'Ссылка должна быть валидным URL',
+          existing_rss: 'RSS уже существует',
+          invalid_rss: 'Ресурс не содержит валидный RSS',
+          network_error: 'Ошибка сети',
+          required_field: 'Не должно быть пустым',
+        },
       },
     },
     news_feed: {
-      feeds: 'Фиды',
-      posts: 'Посты',
-      view: 'Просмотр',
+      feeds: {
+        title: 'Фиды',
+      },
+      posts: {
+        title: 'Посты',
+        view: 'Просмотр',
+      },
     },
   },
 };
