e253a57aa6d64ca6a5b9e65565c71c2b55783fe6
Ajsen Martynov <ajsmartmin@gmail.com>
2024-04-29 08:27:37 +0900

    Refactor src/validator.js

--- src/validator.js
+++ src/validator.js
@@ -12,14 +12,14 @@ yup.setLocale({
   },
 });
 
-export default (newUrl, existingUrls) => {
+export default (newUrl, initialState) => {
+  const existingUrls = initialState.uiState.newsFeed.feeds.map(({ link }) => link);
   const schema = yup
     .string()
     .trim()
     .required()
     .url()
     .notOneOf(existingUrls);
-
   return schema
     .validate(newUrl, { abortEarly: true })
     .then(() => null)
