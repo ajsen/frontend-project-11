d9b2f59d581e29c1a10a824ae257e757e985c671
Ajsen Martynov <ajsmartmin@gmail.com>
2024-05-09 12:35:24 +0900

    Delete src/dataGetter.js and src/dataLoader.js

--- src/dataGetter.js
+++
@@ -1,6 +0,0 @@
-// @ts-check
-
-import loadData from './dataLoader.js';
-import parseData from './dataParser.js';
-
-export default (url) => loadData(url).then((response) => parseData(response.data.contents));
