dc04f799c2512f1eb7490c3bf495cfc3b51f592e
Ajsen Martynov <ajsmartmin@gmail.com>
2024-05-21 00:06:30 +0900

    Delete src/utilities.js

--- src/utilities.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-param-reassign */
-
-export const setElementDisabled = (element, disabled) => {
-  element.disabled = disabled;
-};
-
-export const toggleElementClass = (element, className, force) => {
-  element.classList.toggle(className, force);
-};
